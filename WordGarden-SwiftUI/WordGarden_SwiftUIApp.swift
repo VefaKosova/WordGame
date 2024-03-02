d96fb0d59b2eb733eb6ae37cde7525eb0556b50e
VefaKosova <230316001@ogr.cbu.edu.tr>
2024-03-02 18:59:43 +0300

    Initial Commit

---
+++ WordGarden-SwiftUI/WordGarden_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordGarden_SwiftUIApp.swift
+//  WordGarden-SwiftUI
+//
+//  Created by Vefa Kosova on 2.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct WordGarden_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
